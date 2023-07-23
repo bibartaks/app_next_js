9b4ade6b6cb6d2395d78cc1b6eca91b77e2c93bb
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-07-23 16:53:59 +0600

    Added movie api

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  )
+  const res = await data.json()
+  console.log(res)
+  return (
+    <main>
+      <h1 className="">Hello Next 13 ❤️🔥</h1>
+    </main>
+  )
+}
